49bbc62504106de1445b48b4af7a375933c54ed1
atm-ph <phf@atm-consulting.fr>
2018-07-13 21:43:04 +0200

    Clean module

--- core/modules/moddolishop.class.php
+++ core/modules/moddolishop.class.php
@@ -104,8 +104,8 @@ class moddolishop extends DolibarrModules
 		$this->depends = array();		// List of modules id that must be enabled if this module is enabled
 		$this->requiredby = array();	// List of modules id to disable if this one is disabled
 		$this->conflictwith = array();	// List of modules id this module is in conflict with
-		$this->phpmin = array(5,0);					// Minimum version of PHP required by module
-		$this->need_dolibarr_version = array(3,0);	// Minimum version of Dolibarr required by module
+		$this->phpmin = array(5,3);					// Minimum version of PHP required by module
+		$this->need_dolibarr_version = array(6,0);	// Minimum version of Dolibarr required by module
 		$this->langfiles = array("dolishop@dolishop");
 
 		// Constants
