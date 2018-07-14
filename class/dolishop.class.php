3c172899ae58ded159f3d6c1e8fafefac0b1aec3
atm-ph <phf@atm-consulting.fr>
2018-07-14 20:19:04 +0200

    Update code - add/edit produit OP

--- class/dolishop.class.php
+++ class/dolishop.class.php
@@ -13,7 +13,8 @@ class Dolishop
 	
 	public $result_xml;
 	
-	public $schema_products;
+	public $schema_products_blank;
+	public $schema_products_synopsis;
 
 	public function __construct($db)
 	{
@@ -97,36 +98,6 @@ class Dolishop
 	}
 	
 	
-	
-	/**
-	 * TODO à voir si la méthode reste encore utile
-	 * @return type
-	 */
-	private function getMinMaxPsIdProduct()
-	{
-		$sql = 'SELECT MIN(pe.ps_id_product) as min_ps_id_product, MAX(pe.ps_id_product) as max_ps_id_product';
-		$sql.= ' FROM '.MAIN_DB_PREFIX.'product_extrafields pe';
-		$sql.= ' INNER JOIN '.MAIN_DB_PREFIX.'product p ON (p.rowid = pe.fk_object)';
-		$sql.= ' WHERE p.entity = '.$conf->entity;
-		$resql = $this->db->query($sql);
-		if ($resql)
-		{
-			if ($this->db->num_rows($resql) > 0)
-			{
-				$obj = $this->db->fetch_object($resql);
-				return array($obj->min_ps_id_product, $obj->max_ps_id_product);
-			}
-		}
-		else
-		{
-			$this->error = $this->db->lasterror();
-			$this->errors[] = $this->error;
-		}
-
-		return array(null, null);
-	}
-	
-	
 	/**
 	 * TODO prévoir d'ajouter une restriction du style sur un tag/categ pour éviter d'envoyer toute la base
 	 * Renvoie un tableau contenant les ID produit à synchroniser
@@ -139,6 +110,10 @@ class Dolishop
 		$sql = 'SELECT DISTINCT p.rowid FROM '.MAIN_DB_PREFIX.'product p';
 //		$sql.= ' INNER JOIN '.MAIN_DB_PREFIX.'categorie_product cp ON (cp.fk_product = p.rowid)' // TODO restriction par tags/categories
 //		$sql.= ' ...';
+		
+		// TODO remove
+//		$sql.= ' WHERE p.rowid = 4';
+		
 		
 		$resql = $this->db->query($sql);
 		if ($resql)
@@ -159,8 +134,8 @@ class Dolishop
 	
 	
 	/**
-	 * Permet de récupérer l'id Prestashop d'un objet Dolibarr qui correspond au(x) critère(s) de recherche
-	 * Méthode utilisé par la tâche cron déclaré dans Dolibarr à l'activation du module
+	 * Synchronise les objets vers Prestashop
+	 * Méthode utilisée par la tâche cron déclarée dans Dolibarr à l'activation du module
 	 * 
 	 * @global type $conf
 	 */
@@ -173,112 +148,150 @@ class Dolishop
 		if (!empty($conf->global->DOLISHOP_SYNC_PRODUCTS))
 		{
 			$this->rsyncProducts();
-			
-			
 		}
 		
-		
-		exit;
-		
+		// TODO ... à venir
 	}
 	
-	
+	/**
+	 * Synchronise les produits sur la boutique Prestashop
+	 */
 	private function rsyncProducts()
 	{
 		require_once DOL_DOCUMENT_ROOT.'/product/class/product.class.php';
 		require_once DOL_DOCUMENT_ROOT.'/core/class/extrafields.class.php';
 		
-		$product = new Product($this->db);
+		$dol_product = new Product($this->db);
 		// Je récupère $optionsArray ici pour le faire qu'une seule fois, autrement la méthode "fetch_optionnals()" referait le travail à chaque itération...
 		$extrafields = new ExtraFields($this->db);
-		if (empty($extrafields->attributes[$product->table_element]['loaded'])) $extrafields->fetch_name_optionals_label($product->table_element);
-		$optionsArray = $extrafields->attributes[$product->table_element]['label'];
+		if (empty($extrafields->attributes[$dol_product->table_element]['loaded'])) $extrafields->fetch_name_optionals_label($dol_product->table_element);
+		$optionsArray = $extrafields->attributes[$dol_product->table_element]['label'];
 
-		$this->getSchema('products'); // Load schema pour les add
+		$this->getSchema('products', 'synopsis'); // Load schema pour les add / edit
 	
 		$ProductId = $this->getTProductIdToSync();
 		foreach ($ProductId as $fk_product)
 		{
-			$product = new Product($this->db);
-			if ($product->fetch($fk_product) > 0)
+			$dol_product = new Product($this->db);
+			if ($dol_product->fetch($fk_product) > 0)
 			{
-				if (empty($product->array_options)) $product->fetch_optionals(null, $optionsArray);
+				if (empty($dol_product->array_options)) $dol_product->fetch_optionals(null, $optionsArray);
 
-				$error = false;
-				try
+				if (!empty($dol_product->array_options['options_ps_id_product']))
 				{
-					$opt = array('resource' => 'products', 'schema' => 'synopsis');
-					if (!empty($product->array_options['options_ps_id_product'])) $opt['id'] = $product->array_options['options_ps_id_product'];
-					else $opt['filter[reference]'] = '['.$product->ref.']';
-//var_dump($opt);
-					$this->result_xml = self::$webService->get($opt);
-					$resources = $this->result_xml->children()->children();
+					$opt = array('resource' => 'products', 'filter[id]' => '['.$dol_product->array_options['options_ps_id_product'].']');
+					$alt_opt = array('resource' => 'products', 'filter[reference]' => '['.$dol_product->ref.']');
 				}
-				catch (PrestaShopWebserviceException $e)
+				else
 				{
-					$error = $this->setError($e);
+					$opt = array('resource' => 'products', 'filter[reference]' => '['.$dol_product->ref.']');
+					$alt_opt = array();
 				}
 				
-				if (!$error) // pas de test sur $this->error, car à la 1ère erreur, c'est le restant qui ne sera pas synchro
-				{
-					$nb_products = count($resources->product);
-//					var_dump($nb_products, $resources);exit;
-					if ($nb_products === 1)
-					{
-						// update [edit()]
-					}
-					else if ($nb_products === 0)
-					{
-						// create [add()]
-						$this->addProduct($product);
-exit;
-					}
-					else if ($nb_products > 1)
-					{
-						// Aie, plusieurs occurrences
-					}
-
-
-				}
+				$xml_origin = $this->findPsProductResource($opt, $alt_opt);
+				
+				if ($xml_origin !== false) $this->savePsProduct($dol_product, $xml_origin);
+				else $this->savePsProduct($dol_product);
 			}
-
-
 		}
-		
-exit;
 	}
 	
+	/**
+	 * Fait un appel au webservice Prestashop pour trouver un produit avec $opt ou $opt_alt comme critères
+	 * Attention : cette méthode n'est pas prévu pour fonctionner avec l'argument $opt['id'], car si le produit n'existe pas
+	 *				une erreur 404 est remontée et n'essai pas avec $opt_alt
+	 * 
+	 * Si occurrence trouvée == 1, alors je renvoi $resources
+	 * Si occurrence trouvée == 0 OU > 1, je renvoi false
+	 * 
+	 * @param array $opt
+	 * @param array $alt_opt
+	 * @return SimpleXMLElement|boolean
+	 */
+	private function findPsProductResource($opt, $alt_opt=array())
+	{
+		$error = false;
+		try
+		{
+			if (!isset($opt['limit'])) $opt['limit'] = 2; // ça m'évite de charger des résultats dont je n'ai pas besoin
+			if (!isset($opt['display'])) $opt['display'] = 'full'; // je souhaite récupérer la totalité des champs pour un éventuel update
+			$this->result_xml = self::$webService->get($opt);
+			$resources = $this->result_xml->children();
+		}
+		catch (PrestaShopWebserviceException $e)
+		{
+			$error = $this->setError($e);
+		}
+		
+		if (!$error)
+		{
+			// Nombre d'occurrence ("product") dans "products"
+			if ($resources->children()->count() == 1) return $this->result_xml;
+			else if (!empty($alt_opt)) return $this->findPsProductResource($alt_opt);
+		}
+		
+		return false;
+	}
 	
 	/**
-	 * TODO à voir si je renome pas ça en saveProduct (pour gérer l'update)
+	 * Méthode qui se charge de faire appel au add() ou edit() du webservice
+	 * 
+	 * @global type $mysoc
+	 * @param type $dol_product
+	 * @param type $xml_origin
 	 */
-	private function addProduct(&$product)
+	private function savePsProduct(&$dol_product, $xml_origin=false)
 	{
 		global $mysoc;
 		
-		$schema = clone $this->schema_products;
-		$ps_product = $schema->product;
+		if ($xml_origin !== false) $schema = $xml_origin->children();
+		else $schema = clone $this->schema_products_synopsis;
+
+		// Pour comprendre pourquoi tant de children(), faire un print $schema->asXML()
+		$ps_product = $schema->children()->children();
+		
+		// unset des champs qui sont en readOnly et qu'il faut retirer du xml pour l'envoi au webservice
+		foreach ($this->schema_products_synopsis->children()->children() as $nodeKey => $node)
+		{
+			if ( 
+				isset($node->attributes()->readOnly) && $node->attributes()->readOnly == true
+				|| isset($node->attributes()->read_only) && $node->attributes()->read_only == true)
+			{
+				unset($ps_product->$nodeKey);
+			}
+		}
 //		var_dump($ps_product);exit;
 
-		$ps_product->reference = $product->ref;
-		$ps_product->price =  9.90;
+		$ps_product->reference = $dol_product->ref;
+		$ps_product->price =  $dol_product->price;
 		$ps_product->state =  1;
-
+		
 		foreach ($ps_product->name->children() as $language)
 		{
 			// TODO gérer ici le multi-langs
 			if (self::$TLanguage->{$mysoc->country_code}->id == $language->attributes()->id)
 			{
-				$language[0] = $product->label;
+				$language[0] = $dol_product->label;
 			}
 		}
-
+//		var_dump($ps_product->name->asXML());exit;
+		
 		$error = false;
 		try
 		{
-			$opt = array('resource' => 'products',  'postXml' => $schema->asXML());
-//			echo '<pre>'.htmlspecialchars($schema->asXML(), ENT_QUOTES);exit;
-			$this->result_xml = self::$webService->add($opt);
+			if (!empty($ps_product->id))
+			{
+//				echo '<pre>'.htmlspecialchars($xml_origin->asXML(), ENT_QUOTES);exit;
+				$opt = array('resource' => 'products',  'putXml' => $schema->asXML(), 'id' => $ps_product->id);
+				$this->result_xml = self::$webService->edit($opt);
+			}
+			else
+			{
+//				echo '<pre>'.htmlspecialchars($schema->asXML(), ENT_QUOTES);exit;
+				$opt = array('resource' => 'products',  'postXml' => $schema->asXML());
+				$this->result_xml = self::$webService->add($opt);
+			}
+			
 			$ps_product_return = $this->result_xml->children()->children();
 		}
 		catch (PrestaShopWebserviceException $e)
@@ -288,21 +301,35 @@ exit;
 
 		if (!$error)
 		{
-			$product->array_options['options_ps_id_product'] = (int) $ps_product_return->id;
-			$product->insertExtraFields();
+			$ps_id_product_return = (int) $ps_product_return->id;
+			// Si j'ai fais un update, pas besoin de surcharger avec un save côté extrafields
+			if ($dol_product->array_options['options_ps_id_product'] != $ps_id_product_return)
+			{
+				$dol_product->array_options['options_ps_id_product'] = $ps_id_product_return;
+				$dol_product->insertExtraFields();
+			}
 		}
 	}
 	
 	
-	
-	public function getSchema($resourcename)
+	/**
+	 * Load le schema d'une ressource dans un attribut de l'objet courant sous le format : schema_[$resourcename]_[$type]
+	 * 
+	 * @global type $conf
+	 * @param string	$resourcename	nom de la ressource(products, customers, ... @see admin Prestashop, définition d'une clé webservice (pour la liste complète)
+	 * @param string	$type			blank || synopsis
+	 * @return boolean
+	 */
+	public function getSchema($resourcename, $type='synopsis')
 	{
 		global $conf;
 		
 		$error = false;
 		try
 		{
-			$opt = array('url' => $conf->global->DOLISHOP_PS_SHOP_PATH.'/api/'.$resourcename.'?schema=blank'); // blank, synopsis
+			// "blank" renvoi une sorte de page blanche prête à l'emploi sans les champs 
+			// , alors que "synopsis" donne plus d'infos (type, filtrable, readOnly, ...)
+			$opt = array('url' => $conf->global->DOLISHOP_PS_SHOP_PATH.'/api/'.$resourcename.'?schema='.$type);
 			$this->result_xml = self::$webService->get($opt);
 			$schema = $this->result_xml;
 		}
@@ -314,7 +341,7 @@ exit;
 		if (!$error)
 		{
 			// products ...
-			$this->{'schema_'.$resourcename} = $schema;
+			$this->{'schema_'.$resourcename.'_'.$type} = $schema;
 			return true;
 		}
 		
